3839e77132ea407dc40c51aafb1ab4024b3d5b23
Guilherme Dias <guilhermediasthomaz@hotmail.com>
2024-06-24 19:44:19 -0300

    Payment one-to-one association

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.course.entites.Category;
 import com.educandoweb.course.entites.Order;
 import com.educandoweb.course.entites.OrderItem;
+import com.educandoweb.course.entites.Payment;
 import com.educandoweb.course.entites.Product;
 import com.educandoweb.course.entites.User;
 import com.educandoweb.course.entites.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }
